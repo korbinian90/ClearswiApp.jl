c8c59a907b00eb4fa03011cb66dc55dd6a5945f5
korbinian90 <korbinian90@gmail.com>
2021-09-03 17:14:14 +0200

    more options

--- src/argparse.jl
+++ src/argparse.jl
@@ -22,41 +22,58 @@ function getargs(args::AbstractVector)
             nargs = '+'
         "--mag-combine"
             help = """SNR | average | echo <n> | SE <te>.
-            Magnitude combination algorithm. echo <n> selects a specific echo;
-            SE <te> simulates a single echo scan of the given echo time.
-            """
+                Magnitude combination algorithm. echo <n> selects a specific
+                echo; SE <te> simulates a single echo scan of the given echo
+                time."""
             default = ["SNR"]
             nargs = '+'
         "--mag-sensitivity-correction"
             help = """ <filename> | on | off.
-            Use the CLEAR-SWI sensitivity correction. Alternatively, a
-            sensitivity map can be read from a file"""
+                Use the CLEAR-SWI sensitivity correction. Alternatively, a
+                sensitivity map can be read from a file"""
             default = "on"
-        "--mag-deactivate-softplus-scaling"
-            help = "Deactivate softplus scaling of the magnitude"
-            action = :store_strue
+        "--mag-softplus-scaling"
+            help = """on | off.
+                Set softplus scaling of the magnitude"""
+            default = "on"
         "--unwrapping-algorithm"
             help = """laplacian | romeo | laplacianslice"""
             default = "laplacian"
         "--filter-size"
             help = """Size for the high-pass phase filter in voxels. Can be
-            given as <x> <y> <z> or in array syntax (e.g. [2.2 3.1, 0], which
-            is effectively a 2D filter)."""
+                given as <x> <y> <z> or in array syntax (e.g. [2.2 3.1, 0],
+                which is effectively a 2D filter)."""
             nargs = +
             default = [4,4,0]
         "--phase-scaling-type"
             help = """tanh | negativetanh | positive | negative | triangular
-            Select the type of phase scaling. positive or negative with a 
-            strength of 3-6 is used in standard SWI."""
+                Select the type of phase scaling. positive or negative with a
+                strength of 3-6 is used in standard SWI."""
             default = "tanh"
         "--phase-scaling-strength"
             help = """Sets the phase scaling strength. Corresponds to power
-            values for positive, negative and triangular phase scaling type."""
+                values for positive, negative and triangular phase scaling
+                type."""
             default = 4
+        "--echoes", "-e"
+            help = "Load only the specified echoes from disk"
+            default = [":"]
+            nargs = '+'
+        "--no-mmap", "-N"
+            help = """Deactivate memory mapping. Memory mapping might cause
+                problems on network storage"""
+            action = :store_true
+        "--no-phase-rescale"
+            help = """Deactivate automatic rescaling of phase images. By
+                default the input phase is rescaled to the range [-π;π]."""
+            action = :store_true
         "--writesteps"
             help = """Set to the path of a folder, if intermediate steps should
-            be saved."""
+                be saved."""
             default = nothing
+        "--verbose", "-v"
+            help = "verbose output messages"
+            action = :store_true
     end
     return parse_args(args, s)
 end
@@ -71,7 +88,7 @@ function exception_handler(settings::ArgParseSettings, err, err_code::Int=1)
 end
 
 function getechoes(settings, neco)
-    echoes = eval(Meta.parse(join(settings["unwrap-echoes"], " ")))
+    echoes = eval(Meta.parse(join(settings["echoes"], " ")))
     if echoes isa Int
         echoes = [echoes]
     elseif echoes isa Matrix
@@ -104,24 +121,9 @@ function getTEs(settings, neco, echoes)
     return TEs
 end
 
-function parseweights(settings)
-    if isfile(settings["weights"]) && splitext(settings["weights"])[2] != ""
-        return UInt8.(niread(settings["weights"]))
-    else
-        try
-            reform = "Bool[$(join(collect(settings["weights"]), ','))]"
-            flags = falses(6)
-            flags[1:4] = eval(Meta.parse(reform))
-            return flags
-        catch
-            return Symbol(settings["weights"])
-        end
-    end
-end
-
 function saveconfiguration(writedir, settings, args)
     writedir = abspath(writedir)
-    open(joinpath(writedir, "settings_romeo.txt"), "w") do io
+    open(joinpath(writedir, "settings_clearswi.txt"), "w") do io
         for (fname, val) in settings
             if !(typeof(val) <: AbstractArray)
                 println(io, "$fname: " * string(val))
